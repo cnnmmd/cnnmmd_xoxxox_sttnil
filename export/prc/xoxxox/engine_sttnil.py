82539a1eaa453bbcbffd7af3f513ee6fcf3ebff6
azsgit <10238943+azsgit@users.noreply.github.com>
2025-07-27 22:21:25 +0900

    wip

---
+++ export/prc/xoxxox/engine_sttnil.py
@@ -0,0 +1,11 @@
+from xoxxox.shared import Custom
+
+#---------------------------------------------------------------------------
+
+class SttPrc():
+
+  def __init__(self, config="xoxxox/config_sttnil_000", **dicprm):
+    diccnf = Custom.update(config, dicprm)
+
+  def infere(self, datwav):
+    return ""
